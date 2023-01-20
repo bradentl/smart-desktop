de7c657462da26d53b7524eb74963d2d94dcfb46
bradentl <braden.lee.nyb@gmail.com>
2023-01-20 12:12:28 -0500

    Attempts to merge picamera and face tracking scripts

--- pi_face_tracking.py
+++ pi_face_tracking.py
@@ -1,21 +1,18 @@
 import cv2
+import time
 
-#turn on webcam
-capture = cv2.VideoCapture(0)
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-#The deisred image output width and height
-OUTPUT_SIZE_WIDTH = 775
-OUTPUT_SIZE_HEIGHT = 600
-
-capture.set(3, OUTPUT_SIZE_WIDTH)
-capture.set(4, OUTPUT_SIZE_HEIGHT)
-capture.set(0,150)
+time.sleep(0.1)
  
 # Face Detection model loaded
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-while True:
-    success, img = capture.read()
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
     
     # convert to grayscale
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -29,6 +26,7 @@ while True:
         cv2.putText(img, str(y), (x+w, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
     
     cv2.imshow("image", img)
+    rawCapture.truncate(0)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
